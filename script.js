677467835870bc85218818e9ea0573421b72a7b0
Conrad Houghtby <cxhoughtby@gmail.com>
2021-06-07 14:47:20 -0600

    Add userPlay func

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function computerPlay() {
     const choices = ['Rock', 'Paper', 'Scissors'];
-    const random = choices[Math.floor(Math.random() * choices.length)];
-    return random;
+    const computerSelection = choices[Math.floor(Math.random() * choices.length)];
+    return computerSelection;
 }
 
 console.log(computerPlay());
+
+function userPlay() {
+    const userChoice = prompt('Rock, Paper, or Scissors?').toLowerCase();
+    return userChoice;
+}
+
+console.log(userPlay());
+
