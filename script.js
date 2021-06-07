93466959c51a080c4d45f7c3c8066b749ecdf820
Conrad Houghtby <cxhoughtby@gmail.com>
2021-06-07 16:51:06 -0600

    Refactor functions

--- script.js
+++ script.js
@@ -1,51 +1,46 @@
+
+// This function makes the computer choose either Rock, Paper, or Scissors
 function computerPlay() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
+    const choices = ['rock', 'paper', 'scissors'];
     const computerSelection = choices[Math.floor(Math.random() * choices.length)];
     return computerSelection;
 }
 
+// Make a function that checks the user's selection against the computer's selection and determine a winner. Return a string indicating who wins or if it is a tie.
 
-function userPlay() {
-    const userChoice = prompt('Rock, Paper, or Scissors?').toLowerCase();
-    return userChoice;
-}
-
-
-
-function playGame() {
-    const computerSelection = computerPlay();
-    const userSelection = userPlay();
-
-    if (userSelection === 'rock') {
-        if (computerSelection === 'Scissors') {
-            console.log("You win!");
-        } else if (computerSelection === 'Paper') {
-            console.log("You lose!");
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'scissors') {
+            return 'You win. Rock beats Scissors';
+        } else if (computerSelection === 'paper') {
+            return 'You lose. Paper beats Rock';
         } else {
-            console.log("It's a tie.");
+            return 'It\'s a tie.';
         }
-    } else if (userSelection === 'paper') {
-        if (computerSelection === 'Rock') {
-            console.log("You win!");
-        } else if (computerSelection === 'Scissors') {
-            console.log("You lose!");
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            return 'You win. Paper beats Rock';
+        } else if (computerSelection === 'scissors') {
+            return 'You lose. Scissors beats Paper';
         } else {
-            console.log("It's a tie.");
-        }
-    } else if (userSelection === 'scissors') {
-        if (computerSelection === 'Paper') {
-            console.log("You win!");
-        } else if (computerSelection === 'Rock') {
-            console.log("You lose!");
-        } else {
-            console.log("It's a tie.");
+            return 'It\'s a tie.';
         }
     } else {
-        console.log("Please enter a valid input");
-        playGame();
+        if (computerSelection === 'paper') {
+            return 'You win. Scissors beats Paper';
+        } else if (computerSelection === 'rock') {
+            return 'You lose. Rock beats Scissors';
+        } else {
+            return 'It\'s a tie.';
+        }
     }
-    console.log(`Computer chose: ${computerSelection}`);
-    console.log(`You chose: ${(userSelection)}`);
 }
 
-playGame();
+// Make a function that gets input from the user. This function should be able to only accept a valid input from the user i.e. 'rock' 'paper' or 'scissors'. 
+function playerPlay(playerInput) {
+    return playerInput = prompt('Rock, Paper, or Scissors?').toLowerCase();
+}
+
+const playerSelection = playerPlay();
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
