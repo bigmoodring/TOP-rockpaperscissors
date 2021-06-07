a472156fde54e94ce947a40903191666f9d62f4a
Conrad Houghtby <cxhoughtby@gmail.com>
2021-06-07 14:54:29 -0600

    Add playGame func

--- script.js
+++ script.js
@@ -4,12 +4,48 @@ function computerPlay() {
     return computerSelection;
 }
 
-console.log(computerPlay());
 
 function userPlay() {
     const userChoice = prompt('Rock, Paper, or Scissors?').toLowerCase();
     return userChoice;
 }
 
-console.log(userPlay());
 
+
+function playGame() {
+    const computerSelection = computerPlay();
+    const userSelection = userPlay();
+
+    if (userSelection === 'rock') {
+        if (computerSelection === 'Scissors') {
+            console.log("You win!");
+        } else if (computerSelection === 'Paper') {
+            console.log("You lose!");
+        } else {
+            console.log("It's a tie.");
+        }
+    } else if (userSelection === 'paper') {
+        if (computerSelection === 'Rock') {
+            console.log("You win!");
+        } else if (computerSelection === 'Scissors') {
+            console.log("You lose!");
+        } else {
+            console.log("It's a tie.");
+        }
+    } else if (userSelection === 'scissors') {
+        if (computerSelection === 'Paper') {
+            console.log("You win!");
+        } else if (computerSelection === 'Rock') {
+            console.log("You lose!");
+        } else {
+            console.log("It's a tie.");
+        }
+    } else {
+        console.log("Please enter a valid input");
+        playGame();
+    }
+    console.log(`Computer chose: ${computerSelection}`);
+    console.log(`You chose: ${(userSelection)}`);
+}
+
+playGame();
